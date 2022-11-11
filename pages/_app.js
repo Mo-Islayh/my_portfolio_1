c2907835959d4a818779cd94f4b5ae832c8c38cd
mo islaieh <98762374+moislaieh@users.noreply.github.com>
2022-11-12 02:39:02 +0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import Head from "next/head"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,26 @@ function MyApp({ Component, pageProps }) {
         <title>Mo Islayh</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QZQ23Z8EC6"
+      />
+
+      <Script id="GoogleAnalytics" strategy="lazyOnload">
+        {`
+				var host = window.location.hostname;
+				if (host !== "aa") {
+		
+			  window.dataLayer = window.dataLayer || [];
+			  function gtag() { dataLayer.push(arguments); }
+			  gtag('js', new Date());
+		
+			  gtag('config', 'G-QZQ23Z8EC6');
+			}
+			`}
+      </Script>
+
       <Component {...pageProps} />
     </>
   )
