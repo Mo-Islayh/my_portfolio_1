cee5cd00c49740c15f073161c6b9ecfc7c65562e
mo islaieh <98762374+moislaieh@users.noreply.github.com>
2022-11-26 15:06:44 +0300

    adding redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,30 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/projects/market",
+        destination: "https://market.up.railway.app",
+        permanent: false,
+      },
+      {
+        source: "/projects/speak-up",
+        destination: "https://speak-up.up.railway.app",
+        permanent: false,
+      },
+      {
+        source: "/projects/free4english",
+        destination: "https://free4english.com",
+        permanent: false,
+      },
+      {
+        source: "/projects/my_tube",
+        destination: "https://yt-clone-ecru.vercel.app",
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
