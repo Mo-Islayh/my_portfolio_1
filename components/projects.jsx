dd7df5365232fc334fed5e27a4a11ec0d5c5e697
Mo Islayh <m.islayeh@joacademy.com>
2025-04-02 04:03:14 +0300

    add changes

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,30 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react"
-import { Posts, Skills } from "../content"
-import LinkButton from "./LinkButton"
+import React from "react";
+import { Posts, Skills } from "../content";
 
 const Projects = () => {
   const FindIcon = (tag) => {
-    const x = Skills.find((ss) => ss.name == tag)
+    const x = Skills.find((ss) => ss.name == tag);
     if (x) {
-      return x.icon
+      return x.icon;
     }
-    return <div className="text-xs select-none">{tag}</div>
-  }
+    return <div className="text-xs select-none">{tag}</div>;
+  };
 
   return (
     <div className="flex flex-col max-w-screen-md m-auto w-full mb-4">
-      {/* <div>Full stack</div>
-      <div>Portfolios</div>
-      <div>Moblie Apps</div>
-      <div>Clones</div> */}
-
-      <div className="mb-4 font-bold text-white text-base bg-orange-700 w-fit py-[2px] px-3 rounded-full">
-        Projects
-      </div>
+      <div className="mb-2 font-medium text-white text-2xl">Projects</div>
+      <div className="w-full h-[1px] bg-white my-2"></div>
       {Posts.map((post, _id) => (
         <div key={_id} className="flex flex-col bg-[#333f53] my-2 rounded-xl">
-          <div className="bg-gradient-to-r from-orange-500 to-orange-300 project-backdrop rounded-lg p-4 w-full flex justify-center items-center">
+          <div className="  rounded-lg p-4 w-full flex justify-center items-center">
             <div className=" w-full h-full ">
               <iframe
                 className="rounded-lg"
@@ -42,7 +35,7 @@ const Projects = () => {
           <div className="p-4 flex flex-col gap-4">
             <div className="flex flex-col gap-2">
               <div className="flex justify-between items-center">
-                <div className="font-bold text-white text-base bg-orange-400 w-fit py-[2px] px-3 rounded-full">
+                <div className="font-bold text-white text-xl w-fit py-[2px]  rounded-full">
                   {post.title}
                 </div>
                 <div className="flex gap-3">
@@ -69,9 +62,6 @@ const Projects = () => {
               </div>
             </div>
             <div className="flex flex-col gap-2">
-              <div className="font-bold text-white text-base bg-blue-400 w-fit py-[2px] px-3 rounded-full">
-                Tech used
-              </div>
               <div className="flex flex-wrap gap-2 items-center">
                 {post.tags.map((tag, _id) => (
                   <div
@@ -87,10 +77,10 @@ const Projects = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
 
 const LinkIcon = () => {
   return (
@@ -109,5 +99,5 @@ const LinkIcon = () => {
         d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
       />
     </svg>
-  )
-}
+  );
+};
